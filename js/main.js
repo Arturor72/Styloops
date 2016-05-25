0408fdd947c31f3dc9a9282d0354404e57eb448e
arturor72 <arturor72.jar@gmail.com>
2016-05-24 23:35:12 -0500

    animation and main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $('a.page-scroll[href="#about"]').click(function () {
+        $('#cohete').addClass("testCohete");
+        console.log("Hello")
+    });
+});
